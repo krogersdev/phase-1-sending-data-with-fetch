a29146c05fbe414419ca95e1bed7f6ab69973cc9
Kevin Rogers <kevinarogers1@gmail.com>
2022-01-17 22:19:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+let body = document.querySelector('body');
+
+
+function submitData(name, email) {
+    
+    const user = {
+        name: name, 
+        email: email   
+    };
+
+    const configurationObj = {
+        method: "POST", 
+        headers: {
+            "Content-Type" : "application/json", 
+            Accept: "application/json",
+        }, 
+        body: JSON.stringify(user)
+    }; 
+    
+     return fetch('http://localhost:3000/users', configurationObj)
+        .then(res => res.json())
+        .then((data) => {
+            body.append(data.id)             
+        })
+        .catch((error) => {
+            body.append(error.message);
+            })
+
+              
+    }
+    submitData("Kevin", "kevin@none.com")
